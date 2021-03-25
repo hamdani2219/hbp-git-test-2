4a7e26739536a9527b7171b16042eee0ac68f33a
Hamdani Bayu <hbpgithub@gmail.com>
2021-03-25 12:49:09 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,5 +11,9 @@
   <p>
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga ratione delectus, magnam necessitatibus ea laudantium. Optio sit nam corrupti quibusdam ipsum, natus, architecto inventore provident sequi eligendi neque ab est.
   </p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
